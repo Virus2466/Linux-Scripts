096612cc6ea37649b6f25280c0faa82e20971436
Virus2466 <Pateldev2311@protonmail.com>
2023-08-24 23:08:32 +0530

    modified Graphics.sh

--- Graphics.sh
+++ Graphics.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 
-###########
-#
-# Name : Virus2466
-#
-# Date : 05 - 05 - 23
-#
-# Script for installing essential packages for graphics and gaming 
-#
-##########
+# Script for installing essential packages for graphics and gaming
 
 echo "This is For Arch Distros Only."
 
@@ -22,18 +14,12 @@ sleep 1
 echo "INTEL OR AMD"
 read answer
 
-# Selecting From amd or intel
 
 case $answer in
-
   INTEL)
     sudo pacman -S --needed lib32-vulkan-intel vulkan-intel lib32-vulkan-icd-loader ;;
-
   AMD)
     sudo pacman -S --needed lib32-vulkan-radeon vulkan-radeon lib32-vulkan-icd-loader
-
 esac
-
 #sleep 5
 #notify-send "Graphics.sh" "Task #1 was completed successfully"
-
